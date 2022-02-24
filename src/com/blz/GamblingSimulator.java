7ac8e3005f0e31f6604eaf1893d5cda66feb0b4c
Rohit <rohitkamble10792@gmail.com>
2022-02-24 08:46:14 +0530

    Added:Fisrt commit

---
+++ src/com/blz/GamblingSimulator.java
@@ -0,0 +1,8 @@
+package com.blz;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        public static final int stakeCost = 100;
+        public static final int stakeBetCost = 1;
+    }
+}
